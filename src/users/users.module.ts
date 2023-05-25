4f07ee3d964df91e3621a2329b708c4db64092c9
EloyRocha97 <rochaeloy11@gmail.com>
2023-05-25 20:57:51 -0300

    Edit importacion de provider en module
    edit en module user

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { usuariosProviders } from './Users.privider';
+import { usuariosProviders } from './Users.provider';
 
 @Module({
   providers: [UsersService, ...usuariosProviders],
